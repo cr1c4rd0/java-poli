32efc5970c6e4b595c65da9cc2b4591148c87dc2
Cristian Ricardo <cristianricardo87@gmail.com>
2024-09-03 21:31:42 -0500

    Convesion de medidas

---
+++ Escenarios/src/Escenario3/E22_entrega1.java
@@ -0,0 +1,45 @@
+package Escenario3;
+import java.util.Scanner;
+
+/*Autores:
+  Cristian Mauricio Ricardo Rojas
+
+3. Conversión de medidas de longitud
+Escriba un programa que pida al usuario el número de metros de un objeto, y muestre en
+pantalla las conversiones de dicha cantidad a pies(ft), pulgadas y centímetros.
+
+1ft = 0.3048m
+
+
+
+
+* */
+
+public class E22_entrega1 {
+    public static void main(String[] args) {
+        // Optenemos el valor ingresado por el usuario
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa una medida entera en metros(m): ");
+        int ingresoMetros = entrada.nextInt();
+
+        // LLamamos a calculoPies para realizar la conversion a pies.
+        double pies = calculoPies(ingresoMetros);
+        System.out.println("----------------------------------------------------");
+        System.out.printf("La conversión de metros a pies(ft) es de: %.2f",pies);
+        System.out.println(" ");
+        // Llamamos a calculoPulgadas para realizar a conversion a pulgadas.
+        double pulgadas = calculoPulgadas(ingresoMetros);
+        System.out.printf("La conversión de metros a pulgadas(in) es de: %.2f",pulgadas);
+        System.out.println(" ");
+    }
+
+    static double calculoPies (int ingresoMetros) {
+        double ft = 0.3048; // valor de 1 ft en metros.
+        return (ingresoMetros/ft);
+    }
+
+    static double calculoPulgadas (int ingresoMetros) {
+        double in = 39.37; // valor de 1 in en metros.
+        return (ingresoMetros/in);
+    }
+}
